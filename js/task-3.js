c6527cfeb88c15a594eac78dc377f2641a9b1b00
MazurKateryna <myEmail@example.com>
2020-10-15 13:49:01 +0300

    module-11

--- js/task-3.js
+++ js/task-3.js
@@ -2,25 +2,24 @@ const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
-const makeTransaction = (transaction) => {
+const makeTransaction = transaction => {
   const delay = randomIntegerFromInterval(200, 500);
-  const canProcess = Math.random() > 0.3;
-  return new Promise ((resolve, reject) => {
-    setTimeout(() => {
-  
-      if (canProcess) {
-        resolve(transaction.id, delay);
-      }
 
-      reject(transaction.id);
-      
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const canProcess = Math.random() > 0.3;
+
+      if (canProcess) {
+        resolve({ id: transaction.id, time: delay });
+      } else {
+        reject(transaction.id);
+      }
     }, delay);
-  })
-  
+  });
 };
 
-const logSuccess = (id, time) => {
-  console.log(`Transaction ${id} processed in ${time}ms`);
+const logSuccess = ({ id, time }) => {
+  console.log(`Transaction ${id} processed in ${time} ms`);
 };
 
 const logError = id => {
